15d46e160b621a0955afd86d1f328719f98766d0
Even-dev <samuca1234.oficial@gmail.com>
2024-07-26 21:42:58 -0300

    Atualizar o Cuki.js

--- Cuki.js
+++ Cuki.js
@@ -1,1 +1,2 @@
-console.log("cuki 1.5")
+const agora = new Date();const horas = agora.getHours();
+ const minutos = agora.getMinutes();const horarioAtual = `${horas}:{minutos < 10 ? '0' : ''}${minutos}`;console.log(`Horário atual: {horarioAtual}`);}
